adaf79997adc71a51ef879ce315c034164831240
Bruno Moura <brunomoura.blm@gmail.com>
2021-11-14 14:12:18 -0300

    feat: add global error

---
+++ backend/src/shared/errors/AppError.ts
@@ -0,0 +1,12 @@
+export class AppError extends Error {
+  public readonly statusCode: number
+  public readonly message: string
+
+  constructor (message: string, statusCode = 400) {
+    super(message)
+    this.message = message
+    this.statusCode = statusCode
+
+    this.name = 'AppError'
+  }
+}
