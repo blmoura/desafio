078804d89c871626000a705dff5ccceaf92b0f05
Bruno Moura <brunomoura.blm@gmail.com>
2021-11-15 11:35:00 -0300

    doc: add swagger and documentation the tag api

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -1,13 +1,18 @@
 import 'reflect-metadata'
 import '../typeorm'
 import 'express-async-errors'
+
+import swaggerUI from 'swagger-ui-express'
 import express, { NextFunction, Request, Response } from 'express'
+
+import swaggerFile from '../../../swagger.json'
 import { router } from './routes'
 import { AppError } from '../../errors/AppError'
 
 const app = express()
 app.use(express.json())
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 app.use(router)
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
