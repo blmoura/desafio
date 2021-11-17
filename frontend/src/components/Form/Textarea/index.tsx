8be269919eafc4c622923bad585e7081f9a2623c
Bruno Moura <brunomoura.blm@gmail.com>
2021-11-17 13:26:15 -0300

    feat: add component Form/Textarea

---
+++ frontend/src/components/Form/Textarea/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container } from './styles'
+
+interface IProps {
+  id: string
+  value: string,
+  onChange: any
+}
+
+export const TextArea = ({id, value, onChange}: IProps) => {
+  return (
+    <Container>
+      <label htmlFor={id}>Insight</label>
+      <textarea 
+        id={id} 
+        placeholder="Escreva aqui o seu insight…"
+        value={value}
+        onChange={onChange}
+      ></textarea>
+      <span className="quantity-characters">limite de caracteres: 400</span>
+    </Container>
+  )
+}
---
+++ frontend/src/components/Form/Textarea/styles.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const Container = styled.div`  
+  display: flex;
+  flex-direction: column;
+
+  label {
+    margin-bottom: 2px;
+    font-size: 14px;
+  }
+
+  textarea {
+    height: 13rem;
+    border: none;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+    resize: none;
+    padding-top: 0.5rem;
+    font-size: 16px;
+    margin-bottom: 3rem;
+
+    &::-webkit-input-placeholder {
+      font-size: 1.1rem;
+      font-style: italic;
+    }
+    &:-moz-placeholder {
+      font-size: 1.1rem;
+      font-style: italic;
+    }
+    &::placeholder {
+      font-size: 1.1rem;
+      font-style: italic;
+    }
+
+    &:focus-visible,
+    &:active {
+      border: none;
+      outline: none;
+    }
+
+  }
+
+  .quantity-characters {
+    font-size: 0.875rem;
+    color: #00000040;
+    font-weight: 500;
+    font-style: italic;
+    text-align: right;
+  }
+`
